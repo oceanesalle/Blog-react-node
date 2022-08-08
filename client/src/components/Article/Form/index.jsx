1067ad609b3646078b866edf8686b10ff14c1736
oceansalle <oceanesalleiae@gmail.com>
2022-08-08 10:56:29 +0200

    adding redux

--- client/src/components/Article/Form/index.jsx
+++ client/src/components/Article/Form/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Form extends React.Component {
   constructor(props) {
@@ -16,13 +17,15 @@ class Form extends React.Component {
   }
 
   handleSubmit(){
+    const { onSubmit } = this.props;
     const { title, body, author } = this.state;
 
     return axios.post('http://localhost:8000/api/articles', {
       title,
       body,
       author,
-    });
+    })
+      .then((res) => onSubmit(res.data));
   }
 
   handleChangeField(key, event) {
@@ -54,11 +57,14 @@ class Form extends React.Component {
           className="form-control my-3"
           placeholder="Article Author"
         />
-        <button className="btn btn-primary float-right">Submit</button>
+        <button onClick={this.handleSubmit} className="btn btn-primary float-right">Submit</button>
       </div>
     )
   }
 }
 
-export default Form;
- 
+const mapDispatchToProps = dispatch => ({
+  onSubmit: data => dispatch({ type: 'SUBMIT_ARTICLE', data }),
+});
+
+export default connect(null, mapDispatchToProps)(Form);
